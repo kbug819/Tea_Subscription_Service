2b8674be3bc615a198e12c52503dcb9b02fa2aae
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-21 21:58:00 -0600

    error messages added

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,28 +1,37 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
-    customer = Customer.find(params[:customer_id])
-    subscriptions = customer.subscriptions
-    render json: SubscriptionSerializer.new(subscriptions), status: 200
+    customer = Customer.find_by(id: params[:customer_id])
+    if customer
+      subscriptions = customer.subscriptions
+      render json: SubscriptionSerializer.new(subscriptions), status: 200
+    else
+      render json: { error: 'Incorrect customer id'}, status: 404
+    end
   end
+
   def create
-    tea = Tea.find(params[:tea_id])
-    customer = Customer.find(params[:customer_id])
-    require 'pry';binding.pry
+    tea = Tea.find_by(id: params[:tea_id])
+    customer = Customer.find_by(id: params[:customer_id])
     if customer && tea
       subscription = Subscription.create!(title: "#{tea.title} Subscription", price: tea.price, frequency: (params[:frequency]), tea: tea, customer: customer, sub_status: 1)
       render json: SubscriptionSerializer.new(subscription), status: :created
     else
+      render json: { error: 'Customer id or Tea id incorrect'}, status: 404
     end
   end
 
   def update
-    subscription = Subscription.find(params[:subscription_id])
-    if params[:subscription] == false
-      subscription.update_columns(sub_status: 0)
-      render json: SubscriptionSerializer.new(subscription), status: 200
+    subscription = Subscription.find_by(id: params[:subscription_id])
+    if subscription
+      if params[:subscription] == false
+        subscription.update_columns(sub_status: 0)
+        render json: SubscriptionSerializer.new(subscription), status: 200
+      else
+        subscription.update_columns(sub_status: 1)
+        render json: SubscriptionSerializer.new(subscription), status: 200
+      end
     else
-      subscription.update_columns(sub_status: 1)
-      render json: SubscriptionSerializer.new(subscription), status: 200
+      render json: { error: 'Subscription not found'}, status: 404
     end
   end
 end
